9947f8f7764669ae998b38ddd1612833ff449bed
Vijay Pathak <vpathak@bebotechnologies.com>
2023-07-11 12:11:55 +0530

    Initial Commit

---
+++ BookStore/src/test/java/com/qa/test/testLoginPage.java
@@ -0,0 +1,16 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class testLoginPage {
+	
+	
+	@Test
+	public void checkLoginPageTitle()
+	{
+		
+		System.out.println("Test1");
+		
+	}
+
+}
